52e0c6a96dff1805c46d34e4500a4aed8e3b1c66
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2023-11-17 10:54:37 +0100

    Add files via upload

--- program.py
+++ program.py
@@ -1,21 +1,43 @@
-import cv2
+from PIL import Image
 
-# Charger les images
-image_principale = cv2.imread("DSC_7938.jpg")
-watermark = cv2.imread("water_dark.png", cv2.IMREAD_UNCHANGED)
+# Chemin du dossier contenant les images à traiter
+dossier_images = "images/"
 
-# Définir les coordonnées pour positionner le watermark sur le côté droit
-x_offset = image_principale.shape[1] - watermark.shape[1] - 10
-y_offset = 10
+# Chemin du fichier image de signature (logo)
+chemin_signature = "signature/logo.png"
 
-# Récupérer les dimensions du watermark
-watermark_height, watermark_width, _ = watermark.shape
+# Ouvrir l'image de signature
+signature = Image.open(chemin_signature)
 
-# Superposer le watermark sur l'image principale
-for y in range(watermark_height):
-    for x in range(watermark_width):
-        if watermark[y, x][3] != 0:  # Vérifier la transparence du watermark
-            image_principale[y_offset + y, x_offset + x, :3] = watermark[y, x, :3]
+# Parcourir les images dans le dossier
+import os
 
-# Enregistrer l'image résultante
-cv2.imwrite("image_resultante.jpg", image_principale)
+
+for nom_fichier in os.listdir(dossier_images):
+    chemin_image = os.path.join(dossier_images, nom_fichier)
+
+    # Ouvrir l'image
+    image = Image.open(chemin_image)
+
+    # Obtenir les dimensions de l'image
+    largeur, hauteur = image.size
+
+    # Redimensionner la signature (ajustez la taille selon vos besoins)
+    largeur_signature = int(largeur * 0.1)  # Par exemple, 10 % de la largeur de l'image
+    signature_redimensionnee = signature.resize((largeur_signature, int(largeur_signature / signature.width * signature.height)))
+
+    # Marges
+    marge_x = 20  # Marge horizontale
+    marge_y = 20  # Marge verticale
+
+    # Coordonnées pour placer la signature en bas à droite avec marge
+    coord_x = largeur - signature_redimensionnee.width - marge_x
+    coord_y = hauteur - signature_redimensionnee.height - marge_y
+
+    # Insérer la signature dans le coin inférieur droit
+    image.paste(signature_redimensionnee, (coord_x, coord_y), signature_redimensionnee)
+
+    # Sauvegarder l'image avec la signature
+    image.save(os.path.join(dossier_images, "image_signee_" + nom_fichier))
+
+print("Terminé ! Les images signées ont été enregistrées dans le dossier.")
